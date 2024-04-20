7c1d879a83ac6f110e6e6479b86bc94299fa35ff
CarlosRussell13 <133693000+CarlosRussell13@users.noreply.github.com>
2024-04-20 19:06:31 +0000

    AXIOS INSTALL more

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "index.js",
+    "src/index.js",
+    "src/App.js",
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
